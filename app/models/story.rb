95e5b1ca896281e975fa179454ae5dc440a9761a
Saul Gomez <ing.saul.gomez18@gmail.com>
2018-01-10 19:01:42 -0600

    bugfixing/fix story card in home page

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,5 @@
 class Story < ApplicationRecord
-  has_many :careers
+  has_many :careers, dependent: :destroy
   accepts_nested_attributes_for :careers
   validates :name, :location, :sumary, :ini_age, :sub_age, :ini_title, :ini_career_path, :ini_industry, presence: true
 
