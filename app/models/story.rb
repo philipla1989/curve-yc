1f222bc951f3d2d3348a16942f14d5e24f372bbf
Saul Gomez <ing.saul.gomez18@gmail.com>
2018-01-12 20:54:12 -0600

    Fixes/ Add vanity url

--- app/models/story.rb
+++ app/models/story.rb
@@ -4,4 +4,8 @@ class Story < ApplicationRecord
   validates :name, :location, :sumary, :ini_age, :sub_age, :ini_title, :ini_career_path, :ini_industry, presence: true
 
   scope :sub_career, -> { joins(:careers) }
+
+  def to_param
+    [id, vanity_url.parameterize].join("-")
+  end
 end
