8c26f878bcee3c8c9d67bee7f954e8987a762714
Saul Gomez <ing.saul.gomez18@gmail.com>
2018-02-08 19:00:08 -0600

    edit/homepage-filters

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -42,13 +42,6 @@ class HomeController < ApplicationController
     @search_sub = params[:sub_career_path] == "Anything" ? "" : params[:sub_career_path]
     @stories = Story.where("ini_career_path ilike :search", search: "%#{@search_ini}%")
     @stories = @stories.sub_career.where("careers.name ilike :search", search: "%#{@search_sub}%").uniq
-    if @stories.empty?
-      @stories = Story.where("ini_career_path ilike :search", search: "%#{@search_ini}%").uniq
-      @current = true if @stories.present?
-    elsif @stories.empty?
-      @stories = Story.sub_career.where("careers.name ilike :search", search: "%#{@search_sub}%").uniq
-      @interested = true if @stories.present?
-    end
     @ids = @stories.pluck(:id)
     @search_ini = "Anything" if @search_ini.empty?
     @search_sub = "Anything" if @search_sub.empty?
