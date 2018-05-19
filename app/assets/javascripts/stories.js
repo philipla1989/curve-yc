e1b534a4e2f40e6f39501b794f4eb406481f4122
Saul Gomez <ing.saul.gomez18@gmail.com>
2018-05-19 06:45:08 -0500

    feature/New design story page

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -15,3 +15,38 @@ function bindMultiForms(){
     return event.preventDefault();
   });
 }
+
+function bindAccordion(){
+  var UID = {
+    _current: 0,
+    getNew: function(){
+      this._current++;
+      return this._current;
+    }
+  };
+  HTMLElement.prototype.pseudoStyle = function(element,prop,value){
+    var _this = this;
+    var _sheetId = "pseudoStyles";
+    var _head = document.head || document.getElementsByTagName('head')[0];
+    var _sheet = document.getElementById(_sheetId) || document.createElement('style');
+    _sheet.id = _sheetId;
+    var className = "pseudoStyle" + UID.getNew();
+    
+    _this.className +=  " "+className; 
+    
+    _sheet.innerHTML += " ."+className+":"+element+"{"+prop+":"+value+"}";
+    _head.appendChild(_sheet);
+    return this;
+  };
+
+  $('.collapse').on('show.bs.collapse', function () {    
+    var button = $(this.parentElement.parentElement).children().children().children()[0]
+    button.pseudoStyle("after","transform","rotate(135deg)");    
+    
+  });
+  $('.collapse').on('hidden.bs.collapse', function () {    
+    var button = $(this.parentElement.parentElement).children().children().children()[0]    
+    button.pseudoStyle("after","transform","rotate(45deg)");    
+  });
+  $('.collapse').collapse();
+}
